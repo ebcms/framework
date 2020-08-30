d47f0b4d277c98707d19dd72cfc52be45d671626
ebcms <1540837821@qq.com>
2020-08-30 10:10:27 +0800

    改进..

--- src/App.php
+++ src/App.php
@@ -103,7 +103,7 @@ class App
             $schema = 'http';
         }
 
-        $url_path = parse_url('/' . implode('/', array_filter(explode('/', $_SERVER['REQUEST_URI']))), PHP_URL_PATH);
+        $url_path = '/' . implode('/', array_filter(explode('/', parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH))));
 
         $routeInfo = (function (): Router {
             return $this->container->get(Router::class);
