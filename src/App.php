5a3b37569514162cc2f2c745880d18484dff34d3
ebcms <56634967+ebcms@users.noreply.github.com>
2021-06-29 17:09:59 +0800

    Update App.php

--- src/App.php
+++ src/App.php
@@ -135,38 +135,36 @@ class App
                     Hook $hook
                 ) {
                     $template = new Template($cache);
+                    $template->assign([
+                        'app' => App::getInstance(),
+                        'container' => $container,
+                        'cache' => $container->get(CacheInterface::class),
+                        'logger' => $container->get(LoggerInterface::class),
+                        'request' => $container->get(Request::class),
+                        'session' => $container->get(Session::class),
+                        'config' => $container->get(Config::class),
+                        'router' => $container->get(Router::class),
+                        'hook' => $container->get(Hook::class),
+                    ]);
                     $template->extend('/\{cache\s*(.*)\s*\}([\s\S]*)\{\/cache\}/Ui', function ($matchs) {
                         $params = array_filter(explode(',', trim($matchs[1])));
                         if (!isset($params[0])) {
                             $params[0] = 3600;
                         }
                         if (!isset($params[1])) {
-                            $params[1] = '\'' . md5($matchs[2]) . '\'';
+                            $params[1] = 'tpl_extend_cache_' . md5($matchs[2]);
                         }
-                        return '<?php echo (function(){' .
-                            'return \Ebcms\App::getInstance()->execute(function (
-                                \Psr\SimpleCache\CacheInterface $cache,
-                                \Ebcms\Template $template
-                            ): string {
-                                list($ttl, $id, $tpl, $param)=[' . implode(',', $params) . ',\'' . base64_encode($matchs[2]) . '\'' . ', get_defined_vars()];
-                                if (null === $res = $cache->get(\'tpl_extend_cache_\' . $id)) {
-                                    $res = $template->renderFromString(base64_decode($tpl), $param, $id);
-                                    $cache->set(\'tpl_extend_cache_\' . $id, $res, $ttl);
+                        return '<?php echo call_user_func(function($args){
+                                extract($args);
+                                if (!$cache->has(\'' . $params[1] . '\')) {
+                                    $res = $container->get(\Ebcms\Template::class)->renderFromString(base64_decode(\'' . base64_encode($matchs[2]) . '\'), $args, \'__' . $params[1] . '\');
+                                    $cache->set(\'' . $params[1] . '\', $res, ' . $params[0] . ');
+                                }else{
+                                    $res = $cache->get(\'' . $params[1] . '\');
                                 }
                                 return $res;
-                            });'
-                            . '})();?>';
+                            }, get_defined_vars());?>';
                     });
-
-                    $template->assign([
-                        'app' => App::getInstance(),
-                        'container' => $container,
-                        'request' => $container->get(Request::class),
-                        'config' => $container->get(Config::class),
-                        'router' => $container->get(Router::class),
-                        'hook' => $container->get(Hook::class),
-                        'session' => $container->get(Session::class),
-                    ]);
 
                     /**
                      * @var Mobile_Detect
